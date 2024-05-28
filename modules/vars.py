a6021f2e9f393ac5e6691009e91697ccce8f78ef
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-28 20:23:21 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
 api_id = "4682685"
 api_hash = "3eba5d471162181b8a3f7f5c0a23c307"
-bot_token = "5544919313:AAGeMkIloo3EWXVQmpQQdzMjIQfDKqZ2fro"
+bot_token = "5544919313:AAGVVPEppX10UzzAv5qh0ugbOCt5Slz2XDs"
 likeurl= "https://t.me/movie_time_botonly"
